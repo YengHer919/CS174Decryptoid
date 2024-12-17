fbe7e5f17abf99b6ba8cc8d444023729ebc5cc2a
Yeng Her <119150603+YengHer919@users.noreply.github.com>
2024-12-16 17:42:23 -0800

    Added Double Transposition + Double Transposition Decryption

--- ciphers.php
+++ ciphers.php
@@ -26,9 +26,7 @@
         return $final_text;
 
     }
-    Function doubleTransposition($key){
-        
-    }
+
     Function RC4($text, $key){
         $content = explode("\\n", $text);
         $final = "";
@@ -93,10 +91,116 @@
     function swap(){
         
     }
-    Function doubleTranspositionDecrypt($key){
-        
-    }
+
     Function RC4Decrypt($key){
         
+    }
+
+    function doubleTransposition($key, $message) {
+        // Normalize the key to ensure only alphanumeric characters are used
+        $key = preg_replace('/[^a-zA-Z0-9]/', '', $key);
+        
+        $keyArray = str_split($key);
+        $sortedKey = $keyArray;
+        sort($sortedKey); // Sort the key alphabetically
+        
+        // Create a numeric key mapping based on the sorted order
+        $numericKey = array_map(function($char) use ($sortedKey) {
+            return array_search($char, $sortedKey);
+        }, $keyArray);
+    
+        $numCol = count($numericKey);
+        $numRows = ceil(strlen($message)/$numCol); // Round up number of rows
+    
+        //Build the grid
+        $grid = [];
+         for ($i = 0; $i < $numRows; $i++) {
+             $start = $i * $numCol;
+             $grid[] = str_split(substr($message, $start, $numCol));
+         }
+    
+        // Permute rows
+        $grid = permute($grid, $numericKey);
+    
+        // Permute columns
+        $grid = transpose($grid);
+        $grid = permute($grid, $numericKey);
+        $grid = transpose($grid);
+    
+        // Return grid as string
+        return rtrim(gridToString($grid));
+    }
+    
+    function doubleTranspositionDecrypt($key, $message) {
+        // Normalize the key to ensure only alphanumeric characters are used
+        $key = preg_replace('/[^a-zA-Z0-9]/', '', $key);
+        
+        $keyArray = str_split($key);
+        $sortedKey = $keyArray;
+        sort($sortedKey); // Sort the key alphabetically
+        
+        // Create a numeric key mapping based on the sorted order
+        $numericKey = array_map(function($char) use ($sortedKey) {
+            return array_search($char, $sortedKey);
+        }, $keyArray);
+    
+        $numCol = count($numericKey);
+        $numRows = ceil(strlen($message)/$numCol); // Round up number of rows
+    
+        //Build the grid
+        $grid = [];
+         for ($i = 0; $i < $numRows; $i++) {
+             $start = $i * $numCol;
+             $grid[] = str_split(substr($message, $start, $numCol));
+         }
+    
+        // Inverse operations in reverse order
+        $grid = transpose($grid);
+        $grid = inversePermute($grid, $numericKey);
+        $grid = transpose($grid);
+        
+        $grid = inversePermute($grid, $numericKey);
+    
+        // Return grid as string
+        return rtrim(gridToString($grid));
+    }
+    
+    function inversePermute($grid, $key) {
+        $permutedGrid = [];
+        
+        foreach ($grid as $row) {
+            $newRow = array_fill(0, count($key), ' ');  // Initialize with spaces
+            foreach ($key as $newPos => $oldPos) {
+                if (isset($row[$newPos])) {  // If we have a value at this position
+                    $newRow[$oldPos] = $row[$newPos];
+                }
+            }
+            $permutedGrid[] = $newRow;
+        }
+        return $permutedGrid;
+    }
+     
+    function permute($grid, $key) {
+        $permutedGrid = [];
+        foreach ($grid as $row) {
+            $newRow = [];
+            foreach ($key as $index) {
+                $newRow[] = $row[$index] ?? ' ';
+            }
+            $permutedGrid[] = $newRow;
+        }
+        return $permutedGrid;
+    }
+    
+    function transpose($grid) {
+        $transposed = [];
+        for ($i = 0; $i < count($grid[0]); $i++) {
+            $transposed[$i] = array_column($grid, $i);
+        }
+        return $transposed;
+    }
+   
+    function gridToString($grid) {
+        return implode('', array_map('implode', $grid));
     }
 ?>
